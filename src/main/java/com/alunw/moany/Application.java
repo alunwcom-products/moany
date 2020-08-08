6402f86e199ad9540bde145ea1f51926213c462c
Alun 'Will' Williams <alun@alunw.com>
2020-08-08 17:47:44 +0100

    Added database validator

--- src/main/java/com/alunw/moany/Application.java
+++ src/main/java/com/alunw/moany/Application.java
@@ -6,26 +6,40 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.DatabaseDriver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.jdbc.support.DatabaseStartupValidator;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableAutoConfiguration
 @Import({ SecurityConfig.class })
 public class Application {
-	
-	private static Logger logger = LoggerFactory.getLogger(Application.class);
-	
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(Application.class, args);
-	}
-	
-	@Bean
-	public CommandLineRunner commandLineRunner() {
-		return args -> {
-			logger.debug("Command line runner placeholder.");
-		};
-	}
+
+    private static Logger logger = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            logger.debug("Command line runner placeholder.");
+        };
+    }
+
+    @Bean
+    public DatabaseStartupValidator databaseStartupValidator(DataSource dataSource) {
+        var validator = new DatabaseStartupValidator();
+        validator.setDataSource(dataSource);
+        validator.setInterval(1);
+        validator.setTimeout(180);
+        validator.setValidationQuery(DatabaseDriver.MYSQL.getValidationQuery()); // TODO handle other databases
+        return validator;
+    }
 }
