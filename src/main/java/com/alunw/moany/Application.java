a238eb5c772d95c50188f7bfdc0bd2ec27334455
Alun 'Will' Williams <alun@alunw.com>
2020-08-09 12:09:19 +0100

    Added code to wait for database on startup

--- src/main/java/com/alunw/moany/Application.java
+++ src/main/java/com/alunw/moany/Application.java
@@ -2,6 +2,8 @@ package com.alunw.moany;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -12,7 +14,10 @@ import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.jdbc.support.DatabaseStartupValidator;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+import java.util.Map;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -20,7 +25,13 @@ import javax.sql.DataSource;
 @Import({ SecurityConfig.class })
 public class Application {
 
-    private static Logger logger = LoggerFactory.getLogger(Application.class);
+    @Value("${moany.databaseStartupInterval:5}")
+    private int databaseStartupInterval;
+
+    @Value("${moany.databaseStartupTimeout:120}")
+    private int databaseStartupTimeout;
+
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -35,11 +46,19 @@ public class Application {
 
     @Bean
     public DatabaseStartupValidator databaseStartupValidator(DataSource dataSource) {
-        var validator = new DatabaseStartupValidator();
+        DatabaseStartupValidator validator = new DatabaseStartupValidator();
         validator.setDataSource(dataSource);
-        validator.setInterval(1);
-        validator.setTimeout(180);
-        validator.setValidationQuery(DatabaseDriver.MYSQL.getValidationQuery()); // TODO handle other databases
+        validator.setInterval(databaseStartupInterval);
+        validator.setTimeout(databaseStartupTimeout);
+        validator.setValidationQuery("SELECT 1");
         return validator;
+    }
+
+    @Bean
+    public static BeanFactoryPostProcessor dependsOnPostProcessor() {
+        return bf -> {
+            String[] jpa = bf.getBeanNamesForType(EntityManagerFactory.class);
+            Stream.of(jpa).map(bf::getBeanDefinition).forEach(it -> it.setDependsOn("databaseStartupValidator"));
+        };
     }
 }
