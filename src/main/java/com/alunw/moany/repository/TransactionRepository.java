dc4de04e1113aca3b8611f2347cbba7a59f6718e
Alun 'Will' Williams <alun@alunw.com>
2023-01-22 16:01:09 +0000

    WIP Added controller to output running totals/balance
    ...no tests added!

--- src/main/java/com/alunw/moany/repository/TransactionRepository.java
+++ src/main/java/com/alunw/moany/repository/TransactionRepository.java
@@ -2,6 +2,7 @@ package com.alunw.moany.repository;
 
 import com.alunw.moany.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -9,4 +10,22 @@ public interface TransactionRepository extends JpaRepository<Transaction, String
 	
 	List<Transaction> findAll();
 	List<Transaction> findBySourceName(String sourceName);
+
+	@Query(value = "with data as (\n" +
+			"  select\n" +
+			"    trans_date,\n" +
+			"    if(net_amount > 0, net_amount, 0) as credit,\n" +
+			"    if(net_amount <= 0, net_amount, 0) as debit\n" +
+			"  from transactions\n" +
+			"  order by trans_date\n" +
+			")\n" +
+			"select\n" +
+			"  trans_date,\n" +
+			"  sum(credit) as incoming,\n" +
+			"  sum(debit) as outgoing,\n" +
+			"  sum(credit) + sum(debit) as net,\n" +
+			"  sum(sum(credit) + sum(debit)) over (order by trans_date) as balance\n" +
+			"from data\n" +
+			"group by trans_date", nativeQuery = true)
+	List<Object> findByQuery();
 }
---
+++ src/main/java/com/alunw/moany/rest/RestRunningTotalsController.java
@@ -0,0 +1,32 @@
+package com.alunw.moany.rest;
+
+import com.alunw.moany.repository.TransactionRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * REST controller to return data to construct running balances display.
+ */
+@RestController
+@RequestMapping("/rest/balances/v2/")
+public class RestRunningTotalsController {
+
+    @Autowired
+    private TransactionRepository transactionRepo;
+
+    private static Logger logger = LoggerFactory.getLogger(RestRunningTotalsController.class);
+
+    @RequestMapping(value = {"/"}, method = RequestMethod.GET)
+    @CrossOrigin("*")
+    public List<Object> getTotals() {
+        logger.info("getTotals()");
+        return transactionRepo.findByQuery();
+    }
+}
