44ee8e6ea35b7732cb9df97ad588549f4409287d
Alun 'Will' Williams <alun@alunw.com>
2024-07-08 17:21:24 +0100

    wip

--- src/main/java/EncodePassword.java
+++ src/main/java/EncodePassword.java
@@ -1,15 +1,15 @@
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//
-//public class EncodePassword {
-//
-//	public static void main(String[] args) {
-//		if (args.length != 1) {
-//			System.err.println("EncodePassword requires single argument <plaintext-password>, and outputs encoded password!");
-//		}
-//
-//		PasswordEncoder encoder = new BCryptPasswordEncoder(11);
-//		System.out.println(encoder.encode(args[0]));
-//	}
-//
-//}
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class EncodePassword {
+
+	public static void main(String[] args) {
+		if (args.length != 1) {
+			System.err.println("EncodePassword requires single argument <plaintext-password>, and outputs encoded password!");
+		}
+
+		PasswordEncoder encoder = new BCryptPasswordEncoder(11);
+		System.out.println(encoder.encode(args[0]));
+	}
+
+}
